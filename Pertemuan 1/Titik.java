cf725a8cad79a5b45939589f4dfacc37a0376727
Taraaa <118191070+CaptainRa@users.noreply.github.com>
2024-02-20 11:42:45 +0700

    Add files via upload

--- Pertemuan 1/Titik.java
+++ Pertemuan 1/Titik.java
@@ -1,1 +1,36 @@
+class Titik{
+    //deklarasi atribut
+    double absis;
+    double ordinat;
+    static int counterTitik;
 
+    //deklarasi konstruktor
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik ++;
+    }
+
+    Titik(double x, double y){
+        absis = x;
+        ordinat = y;
+        counterTitik ++;
+    }
+
+    //deklarasi method
+    void setAbsis(double x){
+        absis = x;
+    }
+    void setOrdinat(double y){
+        ordinat = y;
+    }
+    double getAbsis(){
+        return absis;
+    }
+    double getOrdinat(){
+        return ordinat;
+    }
+    int getCounterTitik(){
+        return counterTitik;
+    }
+}
